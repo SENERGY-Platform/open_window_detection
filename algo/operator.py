bfa2d3de4a03ce7e236c80f27bd506146ac7fd64
Christoph Schrade <schrade.christoph@gmail.com>
2024-01-22 13:49:37 +0100

    init

---
+++ algo/operator.py
@@ -0,0 +1,45 @@
+"""
+   Copyright 2022 InfAI (CC SES)
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+__all__ = ("Operator", )
+
+import util
+import os
+from algo import utils
+import pickle
+
+class Operator(util.OperatorBase):
+    def __init__(self, data_path):
+        if not os.path.exists(data_path):
+            os.mkdir(data_path)
+
+        self.sliding_window = [] # This contains the data from the last hour. Entries of the list are pairs of the form {"timestamp": ts, "value": humidity}
+        self.unsusual_drop_detections = []
+        self.unsusual_drop_detections_path = f"{data_path}/unusual_drop_detections.pickle"
+    
+    def run(self, data, selector='energy_func'):
+        current_timestamp = utils.todatetime(data['Energy_Time']).tz_localize(None)
+        new_value = float(data['Energy_Consumption'])
+        print('energy: '+str(new_value)+'  '+'time: '+str(current_timestamp))
+        self.sliding_window = utils.update_sliding_window(self.sliding_window, new_value, current_timestamp)
+        front_mean, front_std, end_mean = utils.compute_front_end_measures(self.sliding_window)
+        if front_mean < end_mean - 2*front_std:
+            self.unsusual_drop_detections.append(current_timestamp)
+            with open(self.unsusual_drop_detections_path) as f:
+                pickle.dump(self.unsusual_drop_detections, f)
+            print("Unusual humidity drop!")
+
+
---
+++ algo/utils.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import numpy as np
+
+
+def todatetime(timestamp):
+    if str(timestamp).isdigit():
+        if len(str(timestamp))==13:
+            return pd.to_datetime(int(timestamp), unit='ms')
+        elif len(str(timestamp))==19:
+            return pd.to_datetime(int(timestamp), unit='ns')
+    else:
+        return pd.to_datetime(timestamp)
+    
+def update_sliding_window(sliding_window, new_value, current_timestamp):
+    first_entry_timestamp = sliding_window[0]["timestamp"]
+    if current_timestamp - first_entry_timestamp > pd.Timedelta(1,"h"):
+        del sliding_window[0]
+    sliding_window.append({"timestamp": current_timestamp, "value": new_value})
+    return sliding_window
+
+def compute_front_end_measures(sliding_window):
+    first_entry_timestamp = sliding_window[0]["timestamp"]
+    last_entry_timestamp = sliding_window[-1]["timestamp"]
+    if last_entry_timestamp - first_entry_timestamp > pd.Timedelta(40, "min"):
+        startindex_of_the_end = len(sliding_window)- len([entry["timestamp"] for entry in sliding_window if last_entry_timestamp - entry["timestamp"] < pd.Timedelta(5, "min")])
+        front_window = [sliding_window[i]["value"] for i in range(startindex_of_the_end)]
+        end_window = [sliding_window[i]["value"] for i in range(startindex_of_the_end, len(sliding_window))]
+        front_mean = np.mean(front_window)
+        front_std = np.std(front_window)
+        end_mean = np.mean(end_window)
+    return front_mean, front_std, end_mean
