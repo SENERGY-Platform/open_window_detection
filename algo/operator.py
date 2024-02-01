f7e1640d79c10f391a03a3cb5823ca8ad8658d90
Christoph Schrade <schrade.christoph@gmail.com>
2024-02-01 10:11:02 +0100

    adjust condition for setting window_closed to false

--- algo/operator.py
+++ algo/operator.py
@@ -56,7 +56,7 @@ class Operator(util.OperatorBase):
             print("Unusual humidity drop!")
             self.window_open = True
         else:
-            if self.window_open and self.sliding_window[-1]["value"] - self.sliding_window[-2]["value"] > 1:
+            if self.window_open and self.sliding_window[-1]["value"] - self.unsusual_drop_detections[-1]["value"] > 1:
                 self.window_open = False
                 self.window_closing_times.append(data['Humidity_Time'])
                 with open(self.window_closing_times_path, "wb") as f:
