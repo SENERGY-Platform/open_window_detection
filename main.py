852cd66e718a6c0d93d8002c6748b6104a7778fd
Christoph Schrade <schrade.christoph@gmail.com>
2024-02-13 15:29:57 +0100

    use analytics-operator-lib-python

--- main.py
+++ main.py
@@ -14,56 +14,79 @@
    limitations under the License.
 """
 
-import util
-import algo
-import json
-import confluent_kafka
-import mf_lib
-import cncr_wdg
-import signal
+__all__ = ("Operator", )
 
-if __name__ == '__main__':
-    util.print_init(name="open-window-operator", git_info_file="git_commit")
-    dep_config = util.DeploymentConfig()
-    opr_config = util.OperatorConfig(json.loads(dep_config.config))
-    util.init_logger(opr_config.config.logger_level)
-    util.logger.debug(f"deployment config: {dep_config}")
-    util.logger.debug(f"operator config: {opr_config}")
-    filter_handler = mf_lib.FilterHandler()
-    for it in opr_config.inputTopics:
-        filter_handler.add_filter(util.gen_filter(input_topic=it, selectors=opr_config.config.selectors, pipeline_id=dep_config.pipeline_id))
-    kafka_brokers = ",".join(util.get_kafka_brokers(zk_hosts=dep_config.zk_quorum, zk_path=dep_config.zk_brokers_path))
-    kafka_consumer_config = {
-        "metadata.broker.list": kafka_brokers,
-        "group.id": dep_config.config_application_id,
-        "auto.offset.reset": dep_config.consumer_auto_offset_reset_config,
-        "max.poll.interval.ms": 6000000
-    }
-    kafka_producer_config = {
-        "metadata.broker.list": kafka_brokers,
-    }
-    util.logger.debug(f"kafka consumer config: {kafka_consumer_config}")
-    util.logger.debug(f"kafka producer config: {kafka_producer_config}")
-    kafka_consumer = confluent_kafka.Consumer(kafka_consumer_config, logger=util.logger)
-    kafka_producer = confluent_kafka.Producer(kafka_producer_config, logger=util.logger)
-    operator = algo.Operator(
-        data_path=opr_config.config.data_path
-    )
-    operator.init(
-        kafka_consumer=kafka_consumer,
-        kafka_producer=kafka_producer,
-        filter_handler=filter_handler,
-        output_topic=dep_config.output,
-        pipeline_id=dep_config.pipeline_id,
-        operator_id=dep_config.operator_id
-    )
-    watchdog = cncr_wdg.Watchdog(
-        monitor_callables=[operator.is_alive],
-        shutdown_callables=[operator.stop],
-        join_callables=[kafka_consumer.close, kafka_producer.flush],
-        shutdown_signals=[signal.SIGTERM, signal.SIGINT, signal.SIGABRT],
-        logger=util.logger
-    )
-    watchdog.start(delay=5)
-    operator.start()
-    watchdog.join()
+from operator_lib.util import OperatorBase
+import os
+import pandas as pd
+from algo import utils
+import pickle
+
+from operator_lib.util import Config
+class CustomConfig(Config):
+    data_path = "/opt/data"
+
+class Operator(OperatorBase):
+    configType = CustomConfig
+    
+    def __init__(self):
+        super().init()
+        data_path = self.config.data_path
+        
+        if not os.path.exists(data_path):
+            os.mkdir(data_path)
+
+        self.first_data_time = None
+
+        self.sliding_window = [] # This contains the data from the last hour. Entries of the list are pairs of the form {"timestamp": ts, "value": humidity}
+        self.unsusual_drop_detections = []
+        self.unsusual_drop_detections_path = f"{data_path}/unusual_drop_detections.pickle"
+
+        if os.path.exists(self.unsusual_drop_detections_path):
+            with open(self.unsusual_drop_detections_path, "rb") as f:
+                self.unsusual_drop_detections = pickle.load(f)
+
+        self.window_open = False
+        self.window_closing_times = []
+        self.window_closing_times_path = f"{data_path}/window_closing_times.pickle" 
+
+        if os.path.exists(self.window_closing_times_path):
+            with open(self.window_closing_times_path, "rb") as f:
+                self.window_closing_times = pickle.load(f)
+    
+    def run(self, data, selector = None):
+        try:
+            current_timestamp = utils.todatetime(data['Humidity_Time']).tz_convert(tz='UTC')
+        except TypeError:
+            current_timestamp = utils.todatetime(data['Humidity_Time']).tz_localize(tz='UTC')
+        if self.first_data_time == None:
+            self.first_data_time = current_timestamp
+        new_value = float(data['Humidity'])
+        print('Humidity: '+str(new_value)+'  '+'Humidity Time: '+str(current_timestamp))
+        self.sliding_window = utils.update_sliding_window(self.sliding_window, new_value, current_timestamp)
+        sampled_sliding_window = utils.minute_resampling(self.sliding_window)
+        front_mean, front_std, end_mean = utils.compute_front_end_measures(sampled_sliding_window)
+        if end_mean < front_mean - 2*front_std and front_mean - end_mean > 2 and self.sliding_window[-1]["value"] < self.sliding_window[-2]["value"]:
+            self.unsusual_drop_detections.append((current_timestamp, new_value))
+            with open(self.unsusual_drop_detections_path, "wb") as f:
+                pickle.dump(self.unsusual_drop_detections, f)
+            print("Unusual humidity drop!")
+            self.window_open = True
+        else:
+            if self.window_open and self.sliding_window[-1]["value"] - self.unsusual_drop_detections[-1][1] > 1:
+                self.window_open = False
+                self.window_closing_times.append((current_timestamp, new_value))
+                with open(self.window_closing_times_path, "wb") as f:
+                    pickle.dump(self.window_closing_times, f)
+                print("Window closed!")
+        if current_timestamp - self.first_data_time < pd.Timedelta(1,'h'):
+            td_until_start = pd.Timedelta(1,'h') - (current_timestamp - self.first_data_time)
+            minutes_until_start = int(td_until_start.total_seconds()/60)
+            return {"window_open": self.window_open, 
+                    "timestamp": str(current_timestamp.tz_localize(None))+"Z",
+                    "initial_phase": f"Die Anwendung befindet sich noch für ca. {minutes_until_start} Minuten in der Initialisierungsphase"}
+        return {"window_open": self.window_open, "timestamp": str(current_timestamp.tz_localize(None))+"Z", "initial_phase": ""}
+    
+from operator_lib.operator_lib import OperatorLib
+if __name__ == "__main__":
+    OperatorLib(Operator(), name="leakage-detection-operator", git_info_file='git_commit')
