c2b4bc7d2be5f4cb758b75640e54860f61b07b5d
Christoph Schrade <schrade.christoph@gmail.com>
2024-02-13 16:14:06 +0100

    fix

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ class CustomConfig(Config):
 class Operator(OperatorBase):
     configType = CustomConfig
     
-    def __init__(self, *args, **kwargs):
+    def init(self, *args, **kwargs):
         super().init(*args, **kwargs)
         data_path = self.config.data_path
         
